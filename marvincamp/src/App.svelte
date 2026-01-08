25ce84f53ab123ac06f3982eba6d9f8d321dac5f
Baptiste R <bapt.rousselot@gmail.com>
2026-01-08 22:44:49 +0100

    init project

---
+++ marvincamp/src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Counter from './lib/Counter.svelte'
+  import Home from './lib/Home.svelte'
+</script>
+
+<main>
+    <Home />
+</main>
---
+++ marvincamp/src/lib/Home.svelte
@@ -0,0 +1,7 @@
+<script>
+    
+</script>
+
+<div>
+    <h1 class='color-white'>Hello World</h1>
+</div>
